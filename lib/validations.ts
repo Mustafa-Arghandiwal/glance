107bcb313c39f4697cff5e161abaef28904206ad
Mustafa-Arghandiwal <mustafaarghandiwal2000@gmail.com>
2026-08-12 03:20:28 +0430

    Add username regex

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,7 +1,8 @@
 import z from 'zod'
 
 export const signUpSchema = z.object({
-    username: z.string().min(3, "Too short").max(20, "Too long"),
+    username: z.string().min(3, "Too short, must be at least 3 characters").max(20, "Too long, must be at most 20 characters")
+        .regex(/^[a-z0-9_]+$/, "Username can only contain lowercase letters, numbers, and underscores"),
     email: z.email(),
     password: z.string().min(8, "Too short"),
     confirmPassword: z.string(),
